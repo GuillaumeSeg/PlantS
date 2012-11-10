2641e5688fab366be06072345f4269143642186d
Bastien <bastien.laby@gmail.com>
2012-11-10 22:55:16 +0100

    Update openGL3 tests

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 PlantS
 ======
-Welcome !
+Welcome on PlantS project.
+
+Developped by :
+	Guillaume Segado,
+	Bastien Laby.
+	
+Tutors : 
+	Vincelas Biri (http://igm.univ-mlv.fr/~biri/),
+	Dominique Revuz (http://www-igm.univ-mlv.fr/~dr/)
+	
+School :
+	IMAC engineering school (IMAC engineering school)
+
+======
+HOW TO INSTALL THE PROJECT :
+
+If you read this document, you should have downloaded the project via GIT.
+
+To make it works, you need the following library :
+	> jogl-2.0
+	> jdom-2.0.3
+These library are available in the project folder.
+
+To install it, to have to add the following JARS to your classpath :
+	
+	> For jogl-2.0 :
+	libjogl/jogamp-all-platforms/jar/gluegen-rt.jar
+	libjogl/jogamp-all-platforms/jar/jogl-all.jar
+	
+	> For jdom-2.0.3
+	jdom-2.0.3/jdom-2.0.3.jar
+
+To finish, you have to link these JARS to the corresponding library.
+All library (.so files) are available in "lib/" folders :
+
+	> For jogl-2.0 :
+	libjogl/jogamp-all-platforms/lib/
+	
+	> For jdom-2.0.3 :
+	jdom-2.0.3/lib/
+	
+======
+Special thanks :
+
+	> IMAC Engineering school
+	> Vinceslas Biri
+	> Dominique Revuz
+	> Laurent Noel (http://etudiant.univ-mlv.fr/~lnoel/index.php?page=contact)
+	> Vincent Nozick (http://igm.univ-mlv.fr/~vnozick/?lang=fr)
--- Sprint 2/src/JOGLRenderer.java
+++ Sprint 2/src/JOGLRenderer.java
@@ -15,37 +15,12 @@ import utils.CST;
 public class JOGLRenderer implements GLEventListener {
 	
 	private ShaderProgram m_ShaderProgram;
+	private int[] m_Vbo;
+	private int[] m_Vao;
 	
-	public void display(GLAutoDrawable drawable) {
-		
-		GL3 gl = drawable.getGL().getGL3();   
-        gl.glClear(GL3.GL_COLOR_BUFFER_BIT | GL3.GL_DEPTH_BUFFER_BIT);
-		System.out.println("GL Error(s) : " + gl.glGetError());
-		
-		gl.glVertexAttribPointer(
-				CST.SHADER_POSITION_LOCATION, // index of attribute into shader
-				3, // number of float for 1 vertex
-				gl.GL_FLOAT, // datatype
-				false, // normalize ?
-				3*CST.SIZEOF_FLOAT, // One vertex total size
-				0 // attribute's offset
-		);
-		
-		// shader code
-		gl.glUseProgram(m_ShaderProgram.p);
-			//start render at 0 vertex go to 3 for a count of 4
-			gl.glDrawRangeElements(GL2.GL_TRIANGLE_STRIP, 0, 3, 4, GL2.GL_UNSIGNED_INT, 0);		
-		gl.glUseProgram(0);
-		
-	}
-
-	public void dispose(GLAutoDrawable drawable) {
-	
-	}
-
 	public void init(GLAutoDrawable drawable) {
 		
-		//setup opengl
+		// Setup opengl
         GL3 gl = drawable.getGL().getGL3();       
         gl.glClearColor(0.0f, 0.2f, 0.2f, 0.0f);        
         gl.glClearDepth(1.0f);          
@@ -55,41 +30,62 @@ public class JOGLRenderer implements GLEventListener {
         
         // Create vertex data         
         float[] vertices = new float[]{
-              -1f, 1f, -2.0f, //0
-              1f, 1f, -2.0f, //1
-              -1f, -1f, -2.0f, //2
-              1f, -1f, -2.0f, //3
+              0.5f, 0.5f,
+              -0.5f, -0.5f
         };
-        ByteBuffer vertexByteBuffer = ByteBuffer.allocateDirect(3 * 4 * 4);            
+        ByteBuffer vertexByteBuffer = ByteBuffer.allocateDirect(vertices.length*CST.SIZEOF_FLOAT);            
         vertexByteBuffer.order(ByteOrder.nativeOrder());      
         FloatBuffer vertexBuffer = vertexByteBuffer.asFloatBuffer();          
         vertexBuffer.put(vertices);
         
-        // Create vertex buffer              
-        int[] vbo = new int[1];         
-        gl.glGenBuffers(1, vbo, 0);        
-        	gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, vbo[0]);      
+        // Create vbo        
+        m_Vbo = new int[1];         
+        gl.glGenBuffers(1, m_Vbo, 0);        
+        	gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, m_Vbo[0]);      
         gl.glBufferData(GL2.GL_ARRAY_BUFFER, vertices.length*CST.SIZEOF_FLOAT, null, GL2.GL_DYNAMIC_DRAW); 
         // Load vertex data into vertex buffer         
         gl.glBufferSubData(GL2.GL_ARRAY_BUFFER, 0, vertexByteBuffer.capacity(), vertexByteBuffer);
         
-        // Create index data           
-        int[] indexes = new int[]{0, 1, 2, 3};
-
-        ByteBuffer indexByteBuffer = ByteBuffer.allocateDirect(4 * 4);     
-        indexByteBuffer.order(ByteOrder.nativeOrder());
-        IntBuffer indexBuffer = indexByteBuffer.asIntBuffer();       
-        indexBuffer.put(indexes);
-
-        // Create index buffer     
-        int[] vao = new int[1];      
-        gl.glGenBuffers(1, vao, 0);
-        gl.glBindBuffer(GL2.GL_ELEMENT_ARRAY_BUFFER, vao[0]);          
-        gl.glBufferData(GL2.GL_ELEMENT_ARRAY_BUFFER, indexes.length*CST.SIZEOF_FLOAT, null, GL2.GL_DYNAMIC_DRAW);       
-        // Load index data into index buffer          
-        gl.glBufferSubData(GL2.GL_ELEMENT_ARRAY_BUFFER, 0, indexByteBuffer.capacity(), indexByteBuffer);
+        // Create vao
+        m_Vao = new int[1];
+        gl.glGenVertexArrays(1, m_Vao, 0);
+        gl.glBindVertexArray(m_Vbo[0]);
+        	gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, m_Vbo[0]);
+        	gl.glEnableVertexAttribArray(CST.SHADER_POSITION_LOCATION);
+        	
+        	gl.glVertexAttribPointer(
+        			CST.SHADER_POSITION_LOCATION, // attribute location into shader
+        			2, // number of element for the attribute
+        			GL2.GL_FLOAT, // data type
+        			false, // normalize ?
+        			2*CST.SIZEOF_FLOAT, // 1 vertex total size
+        			0 // attribute offset      			
+        	);
+        	
+        	gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, 0);
+        gl.glBindVertexArray(0);
+        
         
         m_ShaderProgram = new ShaderProgram(drawable, new File("src/shaders/color.vs.glsl"), new File("src/shaders/color.fs.glsl"));
+	}
+	
+	public void display(GLAutoDrawable drawable) {
+		
+		GL3 gl = drawable.getGL().getGL3();   
+        gl.glClear(GL3.GL_COLOR_BUFFER_BIT | GL3.GL_DEPTH_BUFFER_BIT);
+		System.out.println("GL Error(s) : " + gl.glGetError());
+		
+		// shader code
+		gl.glUseProgram(m_ShaderProgram.p);
+			//gl.glBindVertexArray(m_Vao[0]);
+			gl.glDrawArrays(GL2.GL_POINTS, 0, 2);
+			//gl.glBindVertexArray(0);
+		gl.glUseProgram(0);
+		
+	}
+
+	public void dispose(GLAutoDrawable drawable) {
+	
 	}
 
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
